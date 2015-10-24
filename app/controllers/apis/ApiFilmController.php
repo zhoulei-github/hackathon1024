0467099f01b8e50c9b98a4837959deba7217c6fe
zhoulei <zhoulei@qima-inc.com>
2015-10-24 15:47:41 +0800

    增加影片列表接口

--- app/controllers/apis/ApiFilmController.php
+++ app/controllers/apis/ApiFilmController.php
@@ -1,11 +1,26 @@
 <?php
 
+use \Illuminate\Http\Response;
+use \Illuminate\Support\Facades\Input;
+
 class ApiFilmController extends BaseController
 {
-
-    public function test()
+    public function pageList()
     {
-        return View::make('hello');
-    }
+        $name = Input::get('name', '');
+        $page = Input::get('page', 1);
+        $pageSize = Input::get('page_size', 20);
 
+        $film = Film::where(1);
+
+        if ($name) {
+            $film = $film->where('name', 'like', "%{$name}%");
+        }
+
+        $film = $film->forPage($page, $pageSize);
+
+        $filmData = $film->toArray();
+
+        return Response::json($filmData);
+    }
 }
