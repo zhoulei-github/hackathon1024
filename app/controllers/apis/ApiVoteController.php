6fe3c8e501818297ba632dc59e0972298f542976
wangguangzhou <wangguangzhou@qima-inc.com>
2015-10-24 19:46:19 +0800

    初始化测试数据

--- app/controllers/apis/ApiVoteController.php
+++ app/controllers/apis/ApiVoteController.php
@@ -24,7 +24,7 @@ class ApiVoteController extends ApiBaseController
 
         $voteData = $vote->toArray();
 
-        foreach($voteData as $k=>$v) {
+        foreach ($voteData as $k => $v) {
             $voteData[$k]['cinema']['distance'] = rand(100, 2000);
         }
 
@@ -34,5 +34,38 @@ class ApiVoteController extends ApiBaseController
         ];
 
         return $this->json($rt);
+    }
+
+    /**
+     * 添加测试数据
+     */
+    public function addVotes()
+    {
+        set_time_limit(0);
+        ob_start();
+        $films = Film::where('id', '>', 0)->get()->toArray();
+        foreach ($films as $film) {
+            $num = rand(0, 10);
+            $vote = [];
+            $vote['film_id'] = $film['id'];
+            $vote['price'] = rand(1, 9) . '0';
+            $vote['show_time'] = date('Y-m-d H:i:s', time() + rand(3, 10) * 3600 * 24);
+            $vote['deadline'] = date('Y-m-d H:i:s', strtotime($vote['show_time'])-3600*24);
+            $vote['min_people'] = rand(7,9) . '0';
+            $vote['max_people'] = 100;
+            $vote['status'] = 0;
+            $vote['current_people'] = rand(0, 100);
+
+            for ($i = 0; $i < $num; $i++) {
+                $vote['cinema_id'] = rand(79, 152);
+                Vote::insert($vote);
+                echo '加一条<br>';
+                ob_flush();
+                flush();
+            }
+
+        }
+
+        return $this->json($films);
     }
 }
